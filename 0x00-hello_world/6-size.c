b32c7eb379f8b793787251c48bc1da58f7ee2382
Mohamed_Walid_Fathy <mohamedwalidfathy2005@gmail.com>
2023-09-07 19:34:21 -0700

    final

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+* Description: A C program that prints size of various types using printf function
+*
+*Return: 0
+**/
+int main(void) 
+{
+printf("size of a char: %u byte(s)\n", sizeof(char));
+printf("size of an int: %u byte(s)\n", sizeof(int));
+printf("size of a long int: %u byte(s)\n", sizeof(long int));
+printf("size of a long long int: %u byte(s)\n", sizeof(long long int));
+printf("size of a float: %u byte(s)\n", sizeof(float));
+printf("size of a double: %u byte(s)\n", sizeof(double));
+return (0);
+}
