4f47cdcbc3abea68efd78c907867b9d51b9d2d89
Mohamed_Walid_Fathy <mohamedwalidfathy2005@gmail.com>
2023-09-07 19:04:14 -0700

    task5second

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
-int main() {
-    puts("\"Programming is like building a multilingual puzzle")
-    return 0;
+#include <stdio.h>
+/**
+*main - Entry point
+*
+* Description: A C program that prints with puts function
+*
+*Return: 0
+**/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle")
+return 0;
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
-int main(void)/*main function that prints a sentence*/ 
-{printf("with proper grammar, but the outcome is a piece of art,\n");
-return 0;
+/**
+*main - Entry point
+*
+* Description: A C program that prints with printf function
+*
+*Return: 0
+**/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return(0);
 }
