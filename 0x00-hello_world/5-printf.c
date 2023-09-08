9a4cd6fbd91edf13a3ff5d1a18bf954c7b28489c
Mohamed_Walid_Fathy <mohamedwalidfathy2005@gmail.com>
2023-09-07 18:56:36 -0700

    task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-int main() {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return 0;
+int main(void)/*main function that prints a sentence*/ 
+{printf("with proper grammar, but the outcome is a piece of art,\n");
+return 0;
 }
