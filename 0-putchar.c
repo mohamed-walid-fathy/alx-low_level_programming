7c6ab243d892fc795af3b1734fba00f79c03d336
Mohamed_Walid_Fathy <mohamedwalidfathy2005@gmail.com>
2023-09-12 16:30:30 -0700

    task 0

---
+++ 0-putchar.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+*main - entry point of code
+*
+*Description : prints putchar
+*
+*Return: 0 in case of success
+**/
+int main(void)
+{
+	printf("_putchar\n");
+	return 0;
+}
