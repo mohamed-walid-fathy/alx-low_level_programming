483c679c1f9bb3087c8581f635e4453dd12f8750
Mohamed_Walid_Fathy <mohamedwalidfathy2005@gmail.com>
2023-09-12 16:33:36 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+*main - entry point of code
+*
+*Description : prints the lowercase alphabet
+*
+*Return: 0 in case of success
+**/
+void print_alphabet(void);
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+void print_alphabet(void)
+{
+	int i;
+	for (i = 97; i < 123; i++)
+	{
+		putchar (i);
+	}
+}
