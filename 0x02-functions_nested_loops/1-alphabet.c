c690bec144fc3ec0abcd1d5b54601fb5e0e8302e
Mohamed_Walid_Fathy <mohamedwalidfathy2005@gmail.com>
2023-09-14 00:02:37 +0300

    task 35

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,7 @@
 #include"main.h"
-/**
-*
-*print_alphabet : prints the lowercase alphabet
-*
-**/
+/*
+*print_alphabet - prints the lowercase alphabet
+*/
 void print_alphabet(void)
 {
 	int i;
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include"main.h"
 /*
 *
-* print_alphabet_x10 : prints the lowercase alphabet ten times
+* print_alphabet_x10 - prints the lowercase alphabet ten times
 *
 */
 void print_alphabet_x10(void)
