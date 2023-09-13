07fcfc2f92a384562e7ae596385dd93e07a58214
Mohamed_Walid_Fathy <mohamedwalidfathy2005@gmail.com>
2023-09-12 17:49:59 -0700

    task 28

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,7 +9,7 @@ void print_alphabet_x10(void)
 {       
         int i;
 	int c;
-	for (c = 0;c < 10; c++)
+	for (c = 0;c <= 9; c++)
 	{
         for (i = 97; i < 123; i++)
         {       
--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -6,4 +6,9 @@
  **/
 int _abs(int)
 {
+	if(n < 0)
+	{
+		n = n * -1;
+		return (n);
+	}
 }
