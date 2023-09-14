1d2725cdff481597daa3c38ce4ecbf8148bddaf5
Mohamed_Walid_Fathy <mohamedwalidfathy2005@gmail.com>
2023-09-15 02:24:50 +0300

    task 3sf

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,11 +8,11 @@
 
 void print_numbers(void)
 {
-       char i;
+       int i;
 
-       for (i = '0'; i <= '9'; i++)
+       for (i = 0; i <= 9; i++)
        {
-		_putchar(i);
+		_putchar(i + 48);
        }
 	_putchar('\n');
 }
