1c4b8585f9ef972d0fa58591be03bbf934c80798
Mohamed_Walid_Fathy <mohamedwalidfathy2005@gmail.com>
2023-09-15 02:30:48 +0300

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,21 +1,20 @@
 #include"main.h"
 
 /**
-*print_numbers - prints digits
-*Description: prints numbers from 0 to 9
+*print_most_numbers - prints digits
+*Description: prints digits except 2 and 4
 *Return: void
 */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i;
 
 	for (i = 0; i < 10; i++)
 	{
 	if( i == 2 || i == 4)
-		{
-			i = i;
-		}
+		continue;
+	_putchar(i + 48);
 	}
 	_putchar('\n');
 }
