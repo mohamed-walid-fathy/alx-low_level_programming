a48e169b7b64528e107967fe64d9e5bb650e1472
Mohamed_Walid_Fathy <mohamedwalidfathy2005@gmail.com>
2023-09-15 01:12:25 +0300

    task 0t1

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include"main.h"
+#include<stdio.h>
+/**
+* _isupper - uppercase check
+* @parameterx: Description of parameter x
+* a blank line
+* Description: Longer description of the function)?
+* section header: Section description)*
+* Return: Description of the returned value
+*/
+int _isupper(int c)
+{
+	if( 65 <= c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
