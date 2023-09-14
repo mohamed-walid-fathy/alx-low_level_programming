d7ee7b8744fa4dd49a9eac59d4e6e9da3b5c79dc
Mohamed_Walid_Fathy <mohamedwalidfathy2005@gmail.com>
2023-09-15 01:16:11 +0300

    task 0t3

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,12 @@
 #include"main.h"
-#include<stdio.h>
+
 /**
 * _isupper - uppercase check
 * @c:operand 
 *Description: checks if c is uppercase
 *Return: Description of the returned value
 */
+
 int _isupper(int c)
 {
 	if( 65 <= c <= 90)
