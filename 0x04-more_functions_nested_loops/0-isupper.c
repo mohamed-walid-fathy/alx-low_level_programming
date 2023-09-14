b02b463518135fe6ed4ae98a82308ff9864f6105
Mohamed_Walid_Fathy <mohamedwalidfathy2005@gmail.com>
2023-09-15 01:37:15 +0300

    task0t9

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,12 @@
 #include"main.h"
+
 /**
 *_isupper - uppercase check
 *@c:operand
 *Description: checks if c is uppercase
 *Return: 0: fail ,1: success
 */
+
 int _isupper(int c)
 {
 	if ( c >= 'A' && c <= 'Z')
