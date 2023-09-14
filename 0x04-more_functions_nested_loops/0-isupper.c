95c52f8ecc9b39d5538e0237dac4f953b159dfd2
Mohamed_Walid_Fathy <mohamedwalidfathy2005@gmail.com>
2023-09-15 01:20:43 +0300

    task 0t6

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include"main.h"
 /**
-* _isupper - uppercase check
-* @c:operand 
+*_isupper - uppercase check
+*@c:operand 
 *Description: checks if c is uppercase
 *Return: 0: fail ,1: success
 */
