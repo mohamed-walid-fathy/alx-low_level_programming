2857ea2baa775b60a1924e497d4eb90f0ee54e8b
Mohamed_Walid_Fathy <mohamedwalidfathy2005@gmail.com>
2023-09-16 04:31:21 +0300

    task 12t7

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,33 +9,17 @@
 
 void print_number(int n)
 {
-	unsigned int num;
-	unsigned int reversedNum = 0;
+	unsigned int p = n;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		num = -n;
-	}
-	else
-	{
-		num = n;
+		p = -p;
 	}
 
-	if (num == 0)
+	if ((p / 10) > 0)
 	{
-		_putchar('0');
-		return;
+		print_number(p / 10);
 	}
-	while (num > 0)
-	{
-		reversedNum = reversedNum * 10 + (num % 10);
-		num /= 10;
-	}
-
-	while (reversedNum > 0)
-	{
-		_putchar((reversedNum % 10) + '0');
-		reversedNum /= 10;
-	}
+	_putchar((p % 10) + 48);
 }
