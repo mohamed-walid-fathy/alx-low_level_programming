3eff90b83f08a9fdc5541f3d462c00c36b49854c
Mohamed_Walid_Fathy <mohamedwalidfathy2005@gmail.com>
2023-09-27 05:47:24 +0300

    task 2 and 7 fix

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,18 +9,12 @@
 */
 char *_strchr(char *s, char c)
 {
-	unsigned int i = 0;
+	int i = 0;
 
-	while (*(s + i) != '\0')
+	for (; s[i] >= '\0'; i++)
 	{
-	if (*(s + i) == c)
-		{
-			char *ptr = (char *)(s + i);
-
-			return (ptr);
-		}
-
-		i++;
+		if (s[i] == c)
+			return (&s[i]);
 	}
-	return (NULL);
+	return (0);
 }
