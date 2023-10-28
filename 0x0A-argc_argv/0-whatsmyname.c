98ef4fa851697570bb338b8cb9a1963af3d336ed
Mohamed_Walid_Fathy <mohamedwalidfathy2005@gmail.com>
2023-10-28 13:11:07 +0200

    task 0 t8

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,13 +2,13 @@
 
 /**
 *main - entry point
-*Description: prints program name
 *argc: int of arguments
 *argv: array of arguments
 *Return: Always 0
 */
-int main(int __attribute__((unused)) argc, char *argv[])
+
+int main(int __attribute__((unused)) argc, char const *argv[])
 {
-	printf("%s \n", argv[argc]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
