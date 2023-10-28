dee34a1ed4aa1314a9ce622c3043d4da61a91268
Mohamed_Walid_Fathy <mohamedwalidfathy2005@gmail.com>
2023-10-28 12:57:43 +0200

    task0 t4

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - entry point
- * Description: prints program name
- * Return: Always 0
- */
+*main - entry point
+*Description: prints program name
+*Return: Always 0
+*/
 int main(int argc, char *argv[])
 {
-    argc = 0;
-    printf("%s \n", argv[argc]);
-    return (0);
+	argc = 0;
+	printf("%s \n", argv[argc]);
+	return (0);
 }
