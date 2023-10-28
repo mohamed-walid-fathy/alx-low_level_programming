7e33529c09fdd6e5f3a85625a815d105a7be9d16
Mohamed_Walid_Fathy <mohamedwalidfathy2005@gmail.com>
2023-10-28 18:41:43 +0200

    task 1 t8

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,8 +8,8 @@
 *Return: Always 0
 */
 
-int main(int argc, char __attribute__((unused)) * argv[])
+int main(int argc, char __attribute__((unused))* argv[])
 {
 	printf("%d\n", (argc - 1));
-	return (0);
+	return(0);
 }
