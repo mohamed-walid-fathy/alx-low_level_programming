3391c9d04ca3d59f72cbe70b1a37a4794b1972c0
Mohamed_Walid_Fathy <mohamedwalidfathy2005@gmail.com>
2023-10-28 20:09:19 +0200

    task 4 t1

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+*main - entry point
+*@argc: int of arguments
+*@argv: unused array of arguments
+*Return: 0 for success, 1 for error
+*/
+
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i;
+
+	if (argc < 1)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (atoi(argv[i]) == 0)
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+			{
+				sum += atoi(argv[i]);
+			}
+		}
+		printf("%d\n", sum);
+		return (0);
+	}
+	return (0);
+}
