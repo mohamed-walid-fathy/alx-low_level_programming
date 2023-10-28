dc9d7913102b08a4fb26050863e7b12ec2cba407
Mohamed_Walid_Fathy <mohamedwalidfathy2005@gmail.com>
2023-10-28 12:46:40 +0200

    task 0 t2

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,9 +5,9 @@
  * Description: prints program name
  * Return: Always 0
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     argc = 0;
-    printf("%s \n",argv[argc]);
-	return (0);
+    printf("%s \n", argv[argc]);
+    return (0);
 }
